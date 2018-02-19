d5e1a26fd10f26f493f79bbb68dd89bb4210fb16
opando <opando@MacBook-Pro-de-opando.local>
2018-02-18 21:41:35 -0500

    first commit

---
+++ src/main/java/pe/com/fitpoints/redirect/FitpointsRedirectApplication.java
@@ -0,0 +1,12 @@
+package pe.com.fitpoints.redirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitpointsRedirectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitpointsRedirectApplication.class, args);
+	}
+}
